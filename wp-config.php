c5b6f10167dc823bc73af0ca52ceae562537b000
Josh Wagner <josh@accessu.com>
2024-12-20 10:32:46 -0500

    add ddev wp-config files

--- wp-config.php
+++ wp-config.php
@@ -12,17 +12,10 @@
  * Pantheon platform settings. Everything you need should already be set.
  */
 if (file_exists(dirname(__FILE__) . '/wp-config-pantheon.php') && isset($_ENV['PANTHEON_ENVIRONMENT'])) {
-	require_once(dirname(__FILE__) . '/wp-config-pantheon.php');
-
-/**
- * Local configuration information.
- *
- * If you are working in a local/desktop development environment and want to
- * keep your config separate, we recommend using a 'wp-config-local.php' file,
- * which you should also make sure you .gitignore.
- */
+    require_once(dirname(__FILE__) . '/wp-config-pantheon.php');
+} elseif (file_exists(dirname(__FILE__) . '/wp-config-ddev.php') && !isset($_ENV['PANTHEON_ENVIRONMENT'])){
+    require_once(dirname(__FILE__) . '/wp-config-ddev.php');
 } elseif (file_exists(dirname(__FILE__) . '/wp-config-local.php') && !isset($_ENV['PANTHEON_ENVIRONMENT'])){
-	# IMPORTANT: ensure your local config does not include wp-settings.php
 	require_once(dirname(__FILE__) . '/wp-config-local.php');
 
 /**
